a959d2c7f6b42fe0679f2a1408c78b9bcecea6da
JenOuOb <cucu514@gmail.com>
2022-11-04 09:55:58 +0800

    ABOUT JS+CSS MODAL

--- about.js
+++ about.js
@@ -3,7 +3,7 @@ let btnOrigin = document.querySelector('#btn-origin');
 let btnExperience = document.querySelector('#btn-experience');
 let btnCooperation = document.querySelector('#btn-cooperation');
 // 設定三個modal
-let aboutModal = document.querySelector('#about-modal');
+// let aboutModal = document.querySelector('#about-modal');
 // 設定modal
 let modalOrigin = document.querySelector('#modal-origin');
 let modalExperience = document.querySelector('#modal-experience');
@@ -13,20 +13,20 @@ let modalCloseBtn = document.querySelectorAll('.modal-close-btn');
 
 
 btnOrigin.onclick = function(){
-    aboutModal.style.display='block';
+    // aboutModal.style.display='block';
     modalOrigin.style.display='block';
 }
 btnExperience.onclick = function(){
-    aboutModal.style.display='block';
+    // aboutModal.style.display='block';
     modalExperience.style.display='block';
 }
 
 btnCooperation.onclick = function(){
-    aboutModal.style.display='block';
+    // aboutModal.style.display='block';
     modalCooperation.style.display='block';
 }
 function modalClose(){
-   aboutModal.style.display='none';
+//    aboutModal.style.display='none';
    modalOrigin.style.display='none';
    modalExperience.style.display='none';
    modalCooperation.style.display='none';
